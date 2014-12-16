yaroslavafenkin/xcode-plugin

commit aa9d42d41dd933837902811c9193cd5086d8bf86

Only zip the DSYM if the DSYM file was actually created and fail the build if the operation failed

src/main/java/au/com/rayh/XCodeBuilder.java
@@ -683,10 +683,13 @@ public class XCodeBuilder extends Builder {
                 }
 
                 // also zip up the symbols, if present
-                returnCode = launcher.launch().envs(envs).stdout(listener).pwd(buildDirectory).cmds("ditto", "-c", "-k", "--keepParent", "-rsrc", app.absolutize().getRemote() + ".dSYM", ipaOutputPath.child(baseName + "-dSYM.zip").absolutize().getRemote()).join();
-                if (returnCode > 0) {
-                    listener.getLogger().println(Messages.XCodeBuilder_zipFailed(baseName));
-                    continue;
+                FilePath dSYM = app.withSuffix(".dSYM");
+                if (dSYM.exists()) {
+                    returnCode = launcher.launch().envs(envs).stdout(listener).pwd(buildDirectory).cmds("ditto", "-c", "-k", "--keepParent", "-rsrc", app.absolutize().getRemote(), ipaOutputPath.child(baseName + "-dSYM.zip").absolutize().getRemote()).join();
+                    if (returnCode > 0) {
+                        listener.getLogger().println(Messages.XCodeBuilder_zipFailed(baseName));
+                        return false;
+                    }
                 }
 
                 if(!StringUtils.isEmpty(ipaManifestPlistUrl)) {

commit 29c79831d997ed77477fc3bc6e01731c02ef004f

Fix dSYM issue introduced in aa9d42d41dd

src/main/java/au/com/rayh/XCodeBuilder.java
@@ -701,7 +701,7 @@ public class XCodeBuilder extends Builder {
                 // also zip up the symbols, if present
                 FilePath dSYM = app.withSuffix(".dSYM");
                 if (dSYM.exists()) {
-                    returnCode = launcher.launch().envs(envs).stdout(listener).pwd(buildDirectory).cmds("ditto", "-c", "-k", "--keepParent", "-rsrc", app.absolutize().getRemote(), ipaOutputPath.child(baseName + "-dSYM.zip").absolutize().getRemote()).join();
+                    returnCode = launcher.launch().envs(envs).stdout(listener).pwd(buildDirectory).cmds("ditto", "-c", "-k", "--keepParent", "-rsrc", dSYM.absolutize().getRemote(), ipaOutputPath.child(baseName + "-dSYM.zip").absolutize().getRemote()).join();
                     if (returnCode > 0) {
                         listener.getLogger().println(Messages.XCodeBuilder_zipFailed(baseName));
                         return false;