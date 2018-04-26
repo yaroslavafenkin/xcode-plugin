33308eb5f05f29d41715c4618bbae789c82aaf3d
Kazuhide Takahashi <kazuhide.t@linux-powered.com>
2018-04-26 18:05:27 +0900

    Add switches such as -allowProvisioningUpdates that are not supported by older versions of Xcode.

--- src/main/java/au/com/rayh/XCodeBuilder.java
+++ src/main/java/au/com/rayh/XCodeBuilder.java
@@ -58,6 +58,7 @@ import java.io.IOException;
 import java.io.ObjectStreamException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -685,7 +686,33 @@ public class XCodeBuilder extends Builder implements SimpleBuildStep {
 
             xcodebuildListParser = new XcodeBuildListParser(xcodeBuildListOutput);
         }
+
+        XcodeBuildHelpParser xcodebuildHelpParser;
+        {
+            List<String> commandLine = Lists.newArrayList(getGlobalConfiguration().getXcodebuildPath());
+            commandLine.add("-help");
+            // xcodebuild -help
+            listener.getLogger().println(Messages.XCodeBuilder_DebugInfoAvailableSchemes());
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            returnCode = launcher.launch().envs(envs).cmds(commandLine).stdout(baos).pwd(projectRoot).start().joinWithTimeout(10, TimeUnit.SECONDS, listener);
+            String xcodeBuildHelpOutput = baos.toString("UTF-8");
+            listener.getLogger().println(xcodeBuildHelpOutput);
+            boolean timedOut = returnCode == SIGTERM;
+            if (returnCode > 0 && !timedOut) return false;
+
+            xcodebuildHelpParser = new XcodeBuildHelpParser(xcodeBuildHelpOutput);
+        }
         listener.getLogger().println(Messages.XCodeBuilder_DebugInfoLineDelimiter());
+	Boolean haveAllowProvisioningUpdates = false;
+	if (xcodebuildHelpParser.getParameters().isEmpty()) {
+	    haveAllowProvisioningUpdates = false;
+	}
+	else {
+	    if(Arrays.asList(xcodebuildHelpParser.getParameters()).contains("allowProvisioningUpdates")){
+		haveAllowProvisioningUpdates = true;
+	    }
+	}
 
         // Build
         StringBuilder xcodeReport = new StringBuilder(Messages.XCodeBuilder_invokeXcodebuild());
@@ -804,7 +831,8 @@ public class XCodeBuilder extends Builder implements SimpleBuildStep {
             commandLine.add("DEVELOPMENT_TEAM=" + developmentTeamID);
             xcodeReport.append(", developmentTeamID: ").append(developmentTeamID);
         } else {
-            commandLine.add("-allowProvisioningUpdates");
+	    if (haveAllowProvisioningUpdates)
+		commandLine.add("-allowProvisioningUpdates");
             xcodeReport.append(", developmentTeamID: AUTOMATIC");
         }
 
@@ -948,7 +976,8 @@ public class XCodeBuilder extends Builder implements SimpleBuildStep {
                 packageCommandLine.add(getGlobalConfiguration().getXcodebuildPath());
                 packageCommandLine.addAll(Lists.newArrayList("-exportArchive", "-archivePath", archive.absolutize().getRemote(), "-exportPath", ipaOutputPath.absolutize().getRemote(), "-exportOptionsPlist", exportOptionsPlistLocation.absolutize().getRemote()));
                 if (manualSigning == null || !manualSigning) {
-                    packageCommandLine.add("-allowProvisioningUpdates");
+		    if (haveAllowProvisioningUpdates)
+                	packageCommandLine.add("-allowProvisioningUpdates");
                 }
                 returnCode = launcher.launch().envs(envs).stdout(listener).pwd(projectRoot).cmds(packageCommandLine).join();
                 if (returnCode > 0) {
