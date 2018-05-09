9ce4e2ca1fad58edd09f0f1720b568df704d3b5a
Kazuhide Takahashi <kazuhide.t@linux-powered.com>
2018-05-09 14:00:39 +0900

    Fix 'xcodebuild -help' output parser.

--- src/main/java/au/com/rayh/XCodeBuilder.java
+++ src/main/java/au/com/rayh/XCodeBuilder.java
@@ -669,27 +669,30 @@ public class XCodeBuilder extends Builder implements SimpleBuildStep {
             List<String> commandLine = Lists.newArrayList(getGlobalConfiguration().getXcodebuildPath());
             commandLine.add("-help");
             // xcodebuild -help
-            listener.getLogger().println(Messages.XCodeBuilder_DebugInfoAvailableSchemes());
+            listener.getLogger().println(Messages.XCodeBuilder_DebugInfoAvailableParameters());
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             returnCode = launcher.launch().envs(envs).cmds(commandLine).stdout(baos).pwd(projectRoot).start().joinWithTimeout(10, TimeUnit.SECONDS, listener);
             String xcodeBuildHelpOutput = baos.toString("UTF-8");
-            listener.getLogger().println(xcodeBuildHelpOutput);
+            //listener.getLogger().println(xcodeBuildHelpOutput);
             boolean timedOut = returnCode == SIGTERM;
             if (returnCode > 0 && !timedOut) return false;
 
             xcodebuildHelpParser = new XcodeBuildHelpParser(xcodeBuildHelpOutput);
         }
-        listener.getLogger().println(Messages.XCodeBuilder_DebugInfoLineDelimiter());
 	Boolean haveAllowProvisioningUpdates = false;
-	if (xcodebuildHelpParser.getParameters().isEmpty()) {
+	List<String> availableParameters = xcodebuildHelpParser.getParameters();
+	if (availableParameters.isEmpty()) {
+	    listener.getLogger().println(Messages.XCodeBuilder_NoAvailableParameters());
 	    haveAllowProvisioningUpdates = false;
 	}
 	else {
-	    if(Arrays.asList(xcodebuildHelpParser.getParameters()).contains("allowProvisioningUpdates")){
+	    listener.getLogger().println(StringUtils.join(availableParameters, "\n"));
+	    if(availableParameters.contains("allowProvisioningUpdates")){
 		haveAllowProvisioningUpdates = true;
 	    }
 	}
+	listener.getLogger().println(Messages.XCodeBuilder_DebugInfoLineDelimiter());
 
         // Build
         StringBuilder xcodeReport = new StringBuilder(Messages.XCodeBuilder_invokeXcodebuild());
