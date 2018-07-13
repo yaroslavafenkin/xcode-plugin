92cd81ed0db60cdc4a303b725b45a365cd3e9fd7
Kazuhide Takahashi <kazuhide.t@linux-powered.com>
2018-07-13 16:50:06 +0900

    Improved affinity for Pipeline by using @DataBoundSetter instead of @DataBoundConstructor to get parameters.

--- src/main/java/au/com/rayh/ExportIpa.java
+++ src/main/java/au/com/rayh/ExportIpa.java
@@ -11,9 +11,11 @@ import jenkins.tasks.SimpleBuildStep;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.inject.Inject;
+import javax.annotation.CheckForNull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,54 +24,326 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExportIpa extends Builder implements SimpleBuildStep {
-    public final Boolean buildIpa;
-    public final Boolean generateArchive;
-    public final Boolean noConsoleLog;
-    public final String logfileOutputDirectory;
-    public final Boolean cleanBeforeBuild;
-    public final Boolean cleanTestReports;
-    public final String configuration;
-    public final String target;
-    public final String sdk;
-    public final String xcodeProjectPath;
-    public final String xcodeProjectFile;
-    public final String xcodebuildArguments;
-    public final String cfBundleVersionValue;
-    public final String cfBundleShortVersionStringValue;
-    public final Boolean unlockKeychain;
-    public final String keychainName;
-    public final String keychainPath;
-    public final String keychainPwd;
-    public final String symRoot;
-    public final String xcodeWorkspaceFile;
-    public final String xcodeSchema;
-    public final String archiveDir;
-    public final String developmentTeamName;
-    public final String developmentTeamID;
-    public final Boolean allowFailingBuildResults;
-    public final String ipaName;
-    public final Boolean provideApplicationVersion;
-    public final String ipaOutputDirectory;
-    public final Boolean changeBundleID;
-    public final String bundleID;
-    public final String bundleIDInfoPlistPath;
-    public final Boolean interpretTargetAsRegEx;
-    public final String ipaExportMethod;
-    public final Boolean manualSigning;
-    public final ArrayList<ProvisioningProfile> provisioningProfiles;
-    public final String xcodeName;
-    public final Boolean uploadBitcode;
-    public final Boolean uploadSymbols;
-    public final Boolean compileBitcode;
-    public final String thinning;
-    public final Boolean packResourcesAsset;
-    public final String resourcesAssetURL;
-    public final String appURL;
-    public final String displayImageURL;
-    public final String fullSizeImageURL;
-    public final String assetPackManifestURL;
+    @CheckForNull
+    private String xcodeProjectPath;
+    @CheckForNull
+    private String xcodeProjectFile;
+    private Boolean unlockKeychain;
+    @CheckForNull
+    private String keychainName;
+    @CheckForNull
+    private String keychainPath;
+    @CheckForNull
+    private String keychainPwd;
+    @CheckForNull
+    private String symRoot;
+    @CheckForNull
+    private String xcodeWorkspaceFile;
+    @CheckForNull
+    private String xcodeSchema;
+    @CheckForNull
+    private String archiveDir;
+    @CheckForNull
+    private String developmentTeamName;
+    @CheckForNull
+    private String developmentTeamID;
+    @CheckForNull
+    private String ipaName;
+    @CheckForNull
+    private String ipaOutputDirectory;
+    @CheckForNull
+    private String ipaExportMethod;
+    private Boolean manualSigning;
+    @CheckForNull
+    private ArrayList<ProvisioningProfile> provisioningProfiles;
+    @CheckForNull
+    private String xcodeName;
+    private Boolean uploadBitcode;
+    private Boolean uploadSymbols;
+    private Boolean compileBitcode;
+    @CheckForNull
+    private String thinning;
+    private Boolean embedOnDemandResourcesAssetPacksInBundle;
+    @CheckForNull
+    private String onDemandResourcesAssetPacksBaseURL;
+    @CheckForNull
+    private String appURL;
+    @CheckForNull
+    private String displayImageURL;
+    @CheckForNull
+    private String fullSizeImageURL;
+    @CheckForNull
+    private String assetPackManifestURL;
+
+    @CheckForNull
+    public String getXcodeProjectPath() {
+	return xcodeProjectPath;
+    }
+
+    @DataBoundSetter
+    public void setXcodeProjectPath(String xcodeProjectPath) {
+	this.xcodeProjectPath = xcodeProjectPath;
+    }
+
+    @CheckForNull
+    public String getXcodeProjectFile() {
+	return xcodeProjectFile;
+    }
+
+    public Boolean getUnlockKeychain() {
+	return unlockKeychain;
+    }
+
+    @DataBoundSetter
+    public void setUnlockKeychain(Boolean unlockKeychain) {
+	this.unlockKeychain = unlockKeychain;
+    }
+
+    @CheckForNull
+    public String getKeychainName() {
+	return keychainName;
+    }
+
+    @DataBoundSetter
+    public void setKeychainName(String keychainName) {
+	this.keychainName = keychainName;
+    }
+
+    @CheckForNull
+    public String getKeychainPath() {
+	return keychainPath;
+    }
+
+    @DataBoundSetter
+    public void setKeychainPath(String keychainPath) {
+	this.keychainPath = keychainPath;
+    }
+
+    @CheckForNull
+    public String getKeychainPwd() {
+	return keychainPwd;
+    }
+
+    @DataBoundSetter
+    public void setKeychainPwd(String keychainPwd) {
+	this.keychainPwd = keychainPwd;
+    }
+
+    @CheckForNull
+    public String getSymRoot() {
+	return symRoot;
+    }
+
+    @DataBoundSetter
+    public void setSymRoot(String symRoot) {
+	this.symRoot = symRoot;
+    }
+
+    @CheckForNull
+    public String getXcodeWorkspaceFile() {
+	return xcodeWorkspaceFile;
+    }
+
+    @DataBoundSetter
+    public void setXcodeWorkspaceFile(String xcodeWorkspaceFile) {
+	this.xcodeWorkspaceFile = xcodeWorkspaceFile;
+    }
+
+    @CheckForNull
+    public String getXcodeSchema() {
+	return xcodeSchema;
+    }
+
+    @DataBoundSetter
+    public void setXcodeSchema(String xcodeSchema) {
+	this.xcodeSchema = xcodeSchema;
+    }
+
+    @CheckForNull
+    public String getArchiveDir() {
+	return archiveDir;
+    }
+
+    @DataBoundSetter
+    public void setArchiveDir(String archiveDir) {
+	this.archiveDir = archiveDir;
+    }
+
+    @CheckForNull
+    public String getDevelopmentTeamName() {
+	return developmentTeamName;
+    }
+
+    @DataBoundSetter
+    public void setDevelopmentTeamName(String developmentTeamName) {
+	this.developmentTeamName = developmentTeamName;
+    }
+
+    @CheckForNull
+    public String getDevelopmentTeamID() {
+	return developmentTeamID;
+    }
+
+    @DataBoundSetter
+    public void setDevelopmentTeamID(String developmentTeamID) {
+	this.developmentTeamID = developmentTeamID;
+    }
+
+    @CheckForNull
+    public String getIpaName() {
+	return ipaName;
+    }
+
+    @DataBoundSetter
+    public void setIpaName(String ipaName) {
+	this.ipaName = ipaName;
+    }
+
+    @CheckForNull
+    public String getIpaOutputDirectory() {
+	return ipaOutputDirectory;
+    }
+
+    @DataBoundSetter
+    public void setIpaOutputDirectory(String ipaOutputDirectory) {
+	this.ipaOutputDirectory = ipaOutputDirectory;
+    }
+
+    @CheckForNull
+    public String getIpaExportMethod() {
+	return ipaExportMethod;
+    }
+
+    @DataBoundSetter
+    public void setIpaExportMethod(String ipaExportMethod) {
+	this.ipaExportMethod = ipaExportMethod;
+    }
+
+    public Boolean getManualSigning() {
+	return  manualSigning;
+    }
+
+    @DataBoundSetter
+    public void setManualSigning(Boolean manualSigning) {
+	this.manualSigning = manualSigning;
+    }
+
+    @CheckForNull
+    public ArrayList<ProvisioningProfile> getProvisioningProfiles() {
+	return provisioningProfiles;
+    }
+
+    @DataBoundSetter
+    public void setProvisioningProfiles(ArrayList<ProvisioningProfile> provisioningProfiles) {
+	this.provisioningProfiles = provisioningProfiles;
+    }
+
+    @CheckForNull
+    public String getXcodeName() {
+	return xcodeName;
+    }
+
+    @DataBoundSetter
+    public void setXcodeName(String xcodeName) {
+	this.xcodeName = xcodeName;
+    }
+
+    public Boolean getUploadBitcode() {
+	return uploadBitcode;
+    }
+
+    @DataBoundSetter
+    public void setUploadBitcode(Boolean uploadBitcode) {
+	this.uploadBitcode = uploadBitcode;
+    }
+
+    public Boolean getUploadSymbols() {
+	return uploadSymbols;
+    }
+
+    @DataBoundSetter
+    public void setUploadSymbols(Boolean uploadSymbols) {
+	this.uploadSymbols = uploadSymbols;
+    }
+
+    public Boolean getCompileBitcode() {
+	return compileBitcode;
+    }
+
+    @DataBoundSetter
+    public void setCompileBitcode(Boolean compileBitcode) {
+	this.compileBitcode = compileBitcode;
+    }
+
+    @CheckForNull
+    public String getThinning() {
+	return thinning;
+    }
+
+    @DataBoundSetter
+    public void setThinning(String thinning) {
+	this.thinning = thinning;
+    }
+
+    public Boolean getPackResourcesAsset() {
+	return embedOnDemandResourcesAssetPacksInBundle;
+    }
+
+    @DataBoundSetter
+    public void setPackResourcesAsset(Boolean packResourcesAsset) {
+	this.embedOnDemandResourcesAssetPacksInBundle = packResourcesAsset;
+    }
+
+    @CheckForNull
+    public String getResourcesAssetURL() {
+	return onDemandResourcesAssetPacksBaseURL;
+    }
+
+    @DataBoundSetter
+    public void setResourcesAssetURL(String resourcesAssetURL) {
+	this.onDemandResourcesAssetPacksBaseURL = resourcesAssetURL;
+    }
+
+    @CheckForNull
+    public String getAppURL() {
+	return appURL;
+    }
+
+    @DataBoundSetter
+    public void setAppURL(String appURL) {
+	this.appURL = appURL;
+    }
+
+    @CheckForNull
+    public String getDisplayImageURL() {
+	return displayImageURL;
+    }
+
+    @DataBoundSetter
+    public void setDisplayImageURL(String displayImageURL) {
+	this.displayImageURL = displayImageURL;
+    }
+
+    @CheckForNull
+    public String getFullSizeImageURL() {
+	return fullSizeImageURL;
+    }
+
+    @DataBoundSetter
+    public void setFullSizeImageURL(String fullSizeImageURL) {
+	this.fullSizeImageURL = fullSizeImageURL;
+    }
+
+    @CheckForNull
+    public String getAssetPackManifestURL() {
+	return assetPackManifestURL;
+    }
 
     @DataBoundConstructor
+    public ExportIpa() {
+    }
+
+    @Deprecated
     public ExportIpa(String xcodeProjectPath, String xcodeProjectFile,
                 Boolean unlockKeychain, String keychainName, String keychainPath, String keychainPwd, String symRoot, String xcodeWorkspaceFile,
                 String xcodeSchema, String archiveDir, String developmentTeamName, String developmentTeamID,
@@ -80,38 +354,21 @@ public class ExportIpa extends Builder implements SimpleBuildStep {
 		Boolean packResourcesAsset, String resourcesAssetURL,
 		String appURL, String displayImageURL, String fullSizeImageURL,
 		String assetPackManifestURL) {
-        this.buildIpa = true;
-        this.generateArchive = false;
-        this.noConsoleLog = true;
-        this.logfileOutputDirectory = null;
-        this.sdk = null;
-        this.target = null;
-        this.cleanBeforeBuild = false;
-        this.cleanTestReports = false;
-        this.configuration = null;
+	this();
         this.xcodeProjectPath = xcodeProjectPath;
         this.xcodeProjectFile = xcodeProjectFile;
-        this.xcodebuildArguments = null;
         this.keychainName = keychainName;
         this.xcodeWorkspaceFile = xcodeWorkspaceFile;
         this.xcodeSchema = xcodeSchema;
         this.developmentTeamName = developmentTeamName;
         this.developmentTeamID = developmentTeamID;
-        this.cfBundleVersionValue = null;
-        this.cfBundleShortVersionStringValue = null;
         this.unlockKeychain = unlockKeychain;
         this.keychainPath = keychainPath;
         this.keychainPwd = keychainPwd;
         this.symRoot = symRoot;
         this.archiveDir = archiveDir;
-        this.allowFailingBuildResults = false;
         this.ipaName = ipaName;
         this.ipaOutputDirectory = ipaOutputDirectory;
-        this.provideApplicationVersion = false;
-        this.changeBundleID = false;
-        this.bundleID = null;
-        this.bundleIDInfoPlistPath = null;
-        this.interpretTargetAsRegEx = false;
         this.ipaExportMethod = ipaExportMethod;
         this.manualSigning = manualSigning;
         this.provisioningProfiles = provisioningProfiles;
@@ -120,8 +377,8 @@ public class ExportIpa extends Builder implements SimpleBuildStep {
         this.uploadSymbols = uploadSymbols;
         this.compileBitcode = compileBitcode;
         this.thinning = thinning;
-        this.packResourcesAsset = packResourcesAsset;
-        this.resourcesAssetURL = resourcesAssetURL;
+        this.embedOnDemandResourcesAssetPacksInBundle = packResourcesAsset;
+        this.onDemandResourcesAssetPacksBaseURL = resourcesAssetURL;
         this.appURL = appURL;
         this.displayImageURL = displayImageURL;
         this.fullSizeImageURL = fullSizeImageURL;
@@ -135,15 +392,15 @@ public class ExportIpa extends Builder implements SimpleBuildStep {
 
     @SuppressFBWarnings("DM_DEFAULT_ENCODING")
     private boolean _perform(Run<?,?> build, FilePath filePath, Launcher launcher, EnvVars envs, TaskListener listener) throws InterruptedException, IOException {
-	XCodeBuilder builder = new XCodeBuilder(buildIpa, generateArchive, noConsoleLog, logfileOutputDirectory, cleanBeforeBuild, cleanTestReports, configuration,
-                target, sdk, xcodeProjectPath, xcodeProjectFile, xcodebuildArguments,
-                cfBundleVersionValue, cfBundleShortVersionStringValue, unlockKeychain,
+	XCodeBuilder builder = new XCodeBuilder(true, false, true, null, false, false, null,
+                null, null, xcodeProjectPath, xcodeProjectFile, null,
+                null, null, unlockKeychain,
                 keychainName, keychainPath, keychainPwd, symRoot, xcodeWorkspaceFile,
-                xcodeSchema, archiveDir, developmentTeamName, developmentTeamID, allowFailingBuildResults,
-                ipaName, provideApplicationVersion, ipaOutputDirectory, changeBundleID, bundleID,
-                bundleIDInfoPlistPath, interpretTargetAsRegEx, ipaExportMethod, manualSigning, provisioningProfiles, xcodeName,
+                xcodeSchema, archiveDir, developmentTeamName, developmentTeamID, false,
+                ipaName, false, ipaOutputDirectory, false, null,
+                null, false, ipaExportMethod, manualSigning, provisioningProfiles, xcodeName,
 		uploadBitcode, uploadSymbols, compileBitcode, thinning,
-		packResourcesAsset, resourcesAssetURL,
+		embedOnDemandResourcesAssetPacksInBundle, onDemandResourcesAssetPacksBaseURL,
 		appURL, displayImageURL, fullSizeImageURL, assetPackManifestURL);
 		
 	builder.setSkipBuildStep(true);
